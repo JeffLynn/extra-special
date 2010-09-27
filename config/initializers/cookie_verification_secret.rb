2e1c6d27d0e0ab8d18a3e7646d550894aa94f8cd
Nigel Taylor <nigel.taylor@unboxedconsulting.com>
2010-09-27 10:33:47 +0100

    Created rails framework for Extra Special Trust

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '8854ff6bdf13858acb8afd0cfd90c61f3f62c52884a15e5d4307e362427e5f6836899bf55af262ab1a7d9ea3b4ca657bd01de6e155078eb834377dcdca9ef4a8';
