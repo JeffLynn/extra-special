4986d186cbaa0265043de8fdf71fe3e1aaf61dfb
Borja Arias <borja.arias@unboxedconsulting.com>
2010-09-27 11:21:25 +0100

    Added rspec and cucumber directories structure.

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   
   if %w(development test cucumber).include?(RAILS_ENV)
-    config.gem 'rack',             :lib => false, :version => '1.0.1'
+    config.gem 'rack',             :lib => false, :version => '1.1.0'
     config.gem 'database_cleaner', :lib => false, :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/database_cleaner'))
     config.gem 'rspec',            :lib => false, :version => '>=1.3.0'
     config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2'
