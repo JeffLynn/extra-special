7b8c98eafa86a0fde0b2ac25c3f3ff0e4af23ce3
Petr Zaparka <petr.zaparka@unboxedconsulting.com>
2010-10-19 14:30:45 +0100

    Add rack to gem initialization,

--- config/environment.rb
+++ config/environment.rb
@@ -10,10 +10,10 @@ Rails::Initializer.run do |config|
 
   config.gem 'be_valid_asset',   :lib => false
   config.gem "paperclip"
-
+  config.gem 'rack',             :lib => false, :version => '1.1.0'
+  
   if %w(development test cucumber).include?(RAILS_ENV)
     config.gem 'mime-types',       :lib => false, :version => '= 1.16'
-    config.gem 'rack',             :lib => false, :version => '1.1.0'
     config.gem 'database_cleaner', :lib => false, :version => '>=0.5.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/database_cleaner'))
     config.gem 'rspec',            :lib => false, :version => '>=1.3.0'
     config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2'
